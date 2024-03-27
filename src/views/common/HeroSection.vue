7ac896ee3e19a3ffc62c90f62ce897af18ac20c5
Tarikulat <tarikulat124@gmail.com>
2024-03-27 09:38:25 +0600

    herosection

--- src/views/common/HeroSection.vue
+++ src/views/common/HeroSection.vue
@@ -5,6 +5,8 @@ import { Service, Testimonial } from '@/views/common'
 <template>
     <section
         class="hero-equal-height hig-h section-xl section-header background-video-overly text-white flex-column d-flex justify-content-center">
+        
+        <!-- video section start -->
 
         <!-- <video class="fit-cover w-100 h-100 position-absolute z--1" autoplay muted loop id="myVideo">
             <source src="@/assets/img/video-sr.mp4" type="video/mp4">
